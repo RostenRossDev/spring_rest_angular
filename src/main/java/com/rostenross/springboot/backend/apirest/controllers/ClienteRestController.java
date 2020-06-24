d86da7a99c7f09232cd9361ee5fd58500fc81625
Costantini Nestor Matias <costantini.nestor.m@gmail.com>
2020-06-24 12:13:17 -0300

    Crud was implemented

--- src/main/java/com/rostenross/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/rostenross/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,9 +3,16 @@ package com.rostenross.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rostenross.springboot.backend.apirest.models.entity.Cliente;
@@ -23,5 +30,32 @@ public class ClienteRestController {
 	public List<Cliente> index(){
 		
 		return clienteService.findAll();
+	}
+	
+	
+	@GetMapping("/cliente/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	@PutMapping("/cliente/{id}")
+	public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente clienteActual= clienteService.findById(id);
+		clienteActual.setEmail(cliente.getEmail());
+		clienteActual.setLastname(cliente.getLastname());
+		clienteActual.setName(cliente.getName());
+		return  clienteService.save(clienteActual);
+	}
+	
+	@DeleteMapping("/cliente/{id}")
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public void elete(@PathVariable Long id) {
+		clienteService.delete(id);
 	}
 }
--- src/main/java/com/rostenross/springboot/backend/apirest/models/service/IClienteService.java
+++ src/main/java/com/rostenross/springboot/backend/apirest/models/service/IClienteService.java
@@ -7,4 +7,10 @@ import com.rostenross.springboot.backend.apirest.models.entity.Cliente;
 public interface IClienteService {
 
 	public List<Cliente> findAll();
+	
+	public Cliente save(Cliente cliente);
+	
+	public void delete(Long id);
+	
+	public Cliente findById (Long id);
 }
