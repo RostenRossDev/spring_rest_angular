6dbda7f6253294ba848cf79bb23e7d013834edef
Costantini Nestor Matias <costantini.nestor.m@gmail.com>
2020-07-08 19:58:00 -0300

    Return was changed to ResponseEntity in some function.

--- src/main/java/com/rostenross/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/rostenross/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,9 +1,13 @@
 package com.rostenross.springboot.backend.apirest.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,15 +38,40 @@ public class ClienteRestController {
 	
 	
 	@GetMapping("/clientes/{id}")
-	public Cliente show(@PathVariable Long id) {
-		return clienteService.findById(id);
+	public ResponseEntity<?> show(@PathVariable Long id) {
+		Cliente cliente;
+		Map<String, Object>response= new HashMap<>();
+		try {
+			cliente=clienteService.findById(id);
+
+		} catch (DataAccessException e) {
+			response.put("mensaje","¡¡Error al realizar la consulta en la base de datos!!");
+			response.put("error",e.getMessage().concat(":").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		if (cliente == null) {
+			response.put("mensaje","¡¡El cliente ID:".concat(id.toString().concat(" no existe en la base de datos!!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Cliente>(cliente, HttpStatus.OK); 
 	}
 	
 	@PostMapping("/clientes")
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente create(@RequestBody Cliente cliente) {
-		System.out.println("Cliente Nuevo: "+cliente.getName()+" "+cliente.getLastname()+" "+ cliente.getEmail());
-		return clienteService.save(cliente);
+	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+		Cliente clienteNew;
+		Map<String, Object> response=new HashMap<>();
+		try {
+			clienteNew= clienteService.save(cliente);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "¡¡Error al insertar en la base de datos!!");
+			response.put("error", e.getMessage().concat(":").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		response.put("mensaje", "¡¡El cliente ah sido creado con éxito!!");
+		response.put("cliente", clienteNew);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/cliente/{id}")
